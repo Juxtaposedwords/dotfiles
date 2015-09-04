eaea272ec80f3f8a4089e75a53bef354a9671931
Maloy Wilkes <Maloy.wilkes@gmail.com>
2015-09-04 01:01:49 -0500

    Moved install.d stuff around

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
-#!/usr/bin/env bash
-set -ve
+#!/bin/bash
+set -ev
+
+CONFIG="install.conf.yaml"
+DOTBOT_DIR="dotbot"
+
+DOTBOT_BIN="bin/dotbot"
+BASEDIR="$(cd $HOME/dotfiles && pwd)"
+
+cd "${BASEDIR}"
+git submodule update --init --recursive "${DOTBOT_DIR}"
+
+"${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
 for file in $HOME/dotfiles/install.d/*.sh; do
   bash $file
 done
