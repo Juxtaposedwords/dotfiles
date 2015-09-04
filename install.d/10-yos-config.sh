40fe1b944928b9c23b4c64a2bd2602e1aa2130c7
Maloy Wilkes <Maloy.wilkes@gmail.com>
2015-09-04 11:31:48 -0500

    more tweaks while i try to hunt down the battery settings

--- install.d/10-yos-config.sh
+++ install.d/10-yos-config.sh
@@ -2,6 +2,10 @@
 # Ignore if we're not on OSX
 [[ "$OSTYPE" =~ ^darwin ]] || exit 0
 
+# Show only active apps on teh dock
+defaults write com.apple.dock static-only -bool TRUE; 
+killall Dock
+
 #Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
@@ -22,3 +26,13 @@ defaults write com.apple.finder EmptyTrashSecurely -bool true
 
 # Make Dock icons of hidden applications translucent
 defaults write com.apple.dock showhidden -bool true
+
+# Dim hidden apps
+defaults write com.apple.dock showhidden -bool TRUE; killall Dock
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Faster Dock
+defaults write com.apple.Dock autohide-delay -float 0.1
+defaults write com.apple.dock autohide-time-modifier -float 0.5
