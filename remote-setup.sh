3f2da5c0981615a48cc95403501f5cdc6abec63f
Maloy Wilkes <Maloy.wilkes@gmail.com>
2015-09-04 12:12:57 -0500

    let's see if i can remove homebrew as a part of this install

--- remote-setup.sh
+++ remote-setup.sh
@@ -8,14 +8,14 @@ xcode-select --install
 sudo -v
 
 # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-  brew update
-  brew install git
+# if test ! $(which brew); then
+#   echo "Installing homebrew..."
+#   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# fi
+#   brew update
+#   brew install git
 elif  ( test ! $(which brew); ) &&  [ -f /etc/debian_version ]; then
   apt-get update
   apt-get install git -y
