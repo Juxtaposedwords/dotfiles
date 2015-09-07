ef5085a82e7032cc5fdf30f65580e1b546e2953d
Maloy Wilkes <Maloy.wilkes@gmail.com>
2015-09-07 01:41:24 -0500

    added back dotbot, easier to let them handle symlinks

--- install.d/00-directories.sh
+++ install.d/00-directories.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 mkdir -p $HOME/.shellrc
 mkdir -p $HOME/Downloads/Torrents
+mkdir -p $HOME/Downloads/Torrents/Incomplete
+mkdir -p $HOME/Downloads/Torrents/Complete
 mkdir -p $HOME/Downloads/Chrome
 mkdir -p $HOME/Downloads/Firefox
-mkdir -p $HOME/Downloads/Personal/Development
-mkdir -p $HOME/Downloads/Professional
+mkdir -p $HOME/Documents/Personal/Development
+mkdir -p $HOME/Documents/Professional
 mkdir -p $HOME/Dropbox
 mkdir -p $HOME/Google\ Drive
 mkdir -p $HOME/.shellrc
 cp -rn $HOME/.dotfiles/homeConfigFiles/.config $HOME/.config
 cp  $HOME/.dotfiles/homeConfigFiles/bashrc $HOME/.bashrc
 cp  $HOME/.dotfiles/homeConfigFiles/bash_profile $HOME/.bash_profile
-cp -rn $HOME/homeConfigFiles/.ssh $HOME/.ssh
+cp -rn $HOME/.dotfiles/homeConfigFiles/.ssh $HOME/.ssh
 cp  $HOME/.dotfiles/homeConfigFiles/.gitconfig $HOME/.gitconfig
-ln -s $HOME/.dotfiles/.shellrc/bashrc.d $HOME/.shellrc/bashrc.d 
-ln -s $HOME/.dotfiles/bin $HOME/.shellrc/bin 
-ln -s $HOME/.dotfiles/homeConfigFiles/.vimrc $HOME/.vimrc 
