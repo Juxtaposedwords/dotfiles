44cc04d5f69d4b1aae9415908cb3bf38e8f281b7
juxtaposedwords <juxtaposedwords@gmail.com>
2015-09-13 13:32:19 -0500

    Transmission configuration

--- install.d/30-yos-transmission.sh
+++ install.d/30-yos-transmission.sh
@@ -4,9 +4,17 @@
 
 
 ### Transmission ###
-# Use `~/Documents/Torrents` to store incomplete downloads
-mkdir -p ~/Downloads/Torrents/Incomplete
+
+# Set the directoy for default downloads
+# Default download location
 mkdir -p ~/Downloads/Torrents/Complete
+defaults write org.m0k.transmission DownloadLocationConstant -bool true
+defaults write org.m0k.transmission DownloadChoice -string "Constant"
+defaults write org.m0k.transmission DownloadFolder -string "${HOME}//Downloads/Torrents/Complete"
+
+
+# Use `~/Documents/Torrents/Incomplete` to store incomplete downloads
+mkdir -p ~/Downloads/Torrents/Incomplete
 defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
 defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Torrents/Incomplete"
 
@@ -20,4 +28,3 @@ defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
 defaults write org.m0k.transmission WarningDonate -bool false
 # Hide the legal disclaimer
 defaults write org.m0k.transmission WarningLegal -bool false
-
